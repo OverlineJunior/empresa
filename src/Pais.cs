4340f9c49f9814687960af83280840f3edde42bd
over <franciscosebastianyjunior@hotmail.com>
2023-09-12 21:23:17 -0300

    Implementar pais

---
+++ src/Pais.cs
@@ -0,0 +1,13 @@
+class Pais {
+    public readonly string Nome;
+    public readonly string Sigla;
+    public readonly string Moeda;
+    public readonly int Codigo;
+
+    public Pais(string nome, string sigla, string moeda, int codigo) {
+        Nome = nome;
+        Sigla = sigla;
+        Moeda = moeda;
+        Codigo = codigo;
+    }
+}
