c7d5142ee6f13b086ced36df77dff2e40194bc9b
over <franciscosebastianyjunior@hotmail.com>
2023-09-12 21:24:01 -0300

    Estado possui pais

--- src/Estado.cs
+++ src/Estado.cs
@@ -2,10 +2,12 @@ class Estado {
     public readonly string Nome;
     public readonly string UF;
     public readonly int Codigo;
+    public readonly Pais Pais;
 
-    public Estado(string nome, string uf, int codigo) {
+    public Estado(string nome, string uf, int codigo, Pais pais) {
         Nome = nome;
         UF = uf;
         Codigo = codigo;
+        Pais = pais;
     }
 }
