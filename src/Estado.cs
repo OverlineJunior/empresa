c563cc5a5af7c163a103bd9038aab75313219483
over <franciscosebastianyjunior@hotmail.com>
2023-09-12 21:20:49 -0300

    Implementar estado

---
+++ src/Estado.cs
@@ -0,0 +1,11 @@
+class Estado {
+    public readonly string Nome;
+    public readonly string UF;
+    public readonly int Codigo;
+
+    public Estado(string nome, string uf, int codigo) {
+        Nome = nome;
+        UF = uf;
+        Codigo = codigo;
+    }
+}
