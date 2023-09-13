f7a7fd69aa0f80bc90524d0e93bbf07fff42d65f
over <franciscosebastianyjunior@hotmail.com>
2023-09-12 21:20:38 -0300

    Implementar cidade

---
+++ src/Cidade.cs
@@ -0,0 +1,11 @@
+class Cidade {
+    public readonly string Nome;
+    public readonly string DDD;
+    public readonly int Codigo;
+
+    public Cidade(string nome, string ddd, int codigo) {
+        Nome = nome;
+        DDD = ddd;
+        Codigo = codigo;
+    }    
+}
