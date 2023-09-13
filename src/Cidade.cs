df05914b019468d3b756b56ca9d23cf4c8b47b1e
over <franciscosebastianyjunior@hotmail.com>
2023-09-12 21:21:32 -0300

    Cidade possui estado

--- src/Cidade.cs
+++ src/Cidade.cs
@@ -2,10 +2,12 @@ class Cidade {
     public readonly string Nome;
     public readonly string DDD;
     public readonly int Codigo;
+    public readonly Estado Estado;
 
-    public Cidade(string nome, string ddd, int codigo) {
+    public Cidade(string nome, string ddd, int codigo, Estado estado) {
         Nome = nome;
         DDD = ddd;
         Codigo = codigo;
+        Estado = estado;
     }    
 }
