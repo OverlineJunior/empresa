a8faff9a465484478e93add9d04fed65ddcaf32f
over <franciscosebastianyjunior@hotmail.com>
2023-09-12 21:35:05 -0300

    Pessoa tem cidade

--- src/Pessoal/Pessoa.cs
+++ src/Pessoal/Pessoa.cs
@@ -2,17 +2,20 @@ class Pessoa {
     protected string Nome;
     protected int Idade;
     protected Sexo Sexo;
+    protected Cidade Cidade;
     
-    public Pessoa(string nome, int idade, Sexo sexo) {
+    public Pessoa(string nome, int idade, Sexo sexo, Cidade cidade) {
         Nome = nome;
         Idade = idade;
         Sexo = sexo;
+        Cidade = cidade;
     }
 
     public virtual void Mostrar() {
         Console.WriteLine("[Pessoa]");
-        Console.WriteLine($"Nome:  {Nome}");
-        Console.WriteLine($"Idade: {Idade}");
-        Console.WriteLine($"Sexo:  {Sexo}");
+        Console.WriteLine($"Nome:   {Nome}");
+        Console.WriteLine($"Idade:  {Idade}");
+        Console.WriteLine($"Sexo:   {Sexo}");
+        Console.WriteLine($"Cidade: {Cidade.Nome}");
     }
 }
