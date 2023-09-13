f49b0d3aee8b0fde8e55d77b61b20e83af19a665
over <franciscosebastianyjunior@hotmail.com>
2023-09-12 21:33:45 -0300

    Renomear Sexo_ para Sexo

--- src/Pessoal/Pessoa.cs
+++ src/Pessoal/Pessoa.cs
@@ -1,18 +1,18 @@
 class Pessoa {
     protected string Nome;
     protected int Idade;
-    protected Sexo Sexo_;
+    protected Sexo Sexo;
     
     public Pessoa(string nome, int idade, Sexo sexo) {
         Nome = nome;
         Idade = idade;
-        Sexo_ = sexo;
+        Sexo = sexo;
     }
 
     public virtual void Mostrar() {
         Console.WriteLine("[Pessoa]");
         Console.WriteLine($"Nome:  {Nome}");
         Console.WriteLine($"Idade: {Idade}");
-        Console.WriteLine($"Sexo:  {Sexo_}");
+        Console.WriteLine($"Sexo:  {Sexo}");
     }
 }
